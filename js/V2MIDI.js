0ad6b8f4ed36b98218db36c7016b161ea7476b13
Kay Sievers <kay@versioduo.com>
2023-06-10 22:24:31 +0200

    Update V2MIDI

--- js/V2MIDI.js
+++ js/V2MIDI.js
@@ -7,18 +7,26 @@ class V2MIDI {
   // The octave numbers -2 to 8 are not defined by MIDI itself, it's just what
   // some vendors of instruments and audio workstation software use. The middle
   // C (Number === 60) in this mapping is C3.
-  static Note = Object.freeze({
-    names: ['C', 'C♯', 'D', 'D♯', 'E', 'F', 'F♯', 'G', 'G♯', 'A', 'A♯', 'B'],
+  static Note = class {
+    static names = ['C', 'C♯', 'D', 'D♯', 'E', 'F', 'F♯', 'G', 'G♯', 'A', 'A♯', 'B'];
 
-    name: (note) => {
+    static getName(note) {
       const octave = Math.trunc(note / 12) - 2;
-      return this.Note.names[note % 12] + octave;
-    },
-
-    isBlack: (note) => {
-      return this.Note.names[note % 12].includes('♯');
+      return this.names[note % 12] + octave;
     }
-  });
+
+    static isBlack(note) {
+      return this.names[note % 12].includes('♯');
+    }
+
+    static getOctave(note) {
+      return Math.trunc(note / 12) - 2;
+    }
+
+    static getNote(octave, index = 0) {
+      return ((octave + 2) * 12) + index;
+    }
+  };
 
   // MIDI Control Change (CC) values.
   static CC = Object.freeze({
@@ -68,11 +76,11 @@ class V2MIDI {
     generalPurpose4LSB: 51,
 
     // Single-byte Controllers.
-    sustain: 64,
+    sustainPedal: 64,
     portamento: 65,
     sostenuto: 66,
-    soft: 67,
-    legato: 68,
+    softPedal: 67,
+    legatoPedal: 68,
     hold2: 69,
     soundController1: 70, // Sound Variation
     soundController2: 71, // Timber / Harmonic Intensity
@@ -154,11 +162,11 @@ class V2MIDI {
       17: 'General 2',
       18: 'General 3',
       19: 'General 4',
-      64: 'Sustain',
+      64: 'Sustain Pedal',
       65: 'Portamento',
       66: 'Sostenuto',
       67: 'Soft Pedal',
-      68: 'Legato',
+      68: 'Legato Pedal',
       69: 'Hold 2',
       70: 'Sound 1',
       71: 'Sound 2',
--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 const name = 'axis3d';
-const version = 33;
+const version = 37;
 const files = [
   './',
   'css/bulma-addons.css',
